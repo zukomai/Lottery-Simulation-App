6033631a97f0dff4cdcd3673e11fe6af99cd8509
zukomai <firenationllama@gmail.com>
2022-09-10 14:45:19 -0700

    add function for making lottery ticket

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 // Lottery Simulation App
 
+
+
 // Function that picks 6 random numbers between 1-45.
 function getRandomNumber(min, max) {
     let range = max - min + 1; // gets the range between min and max and adds one for inclusiveness
@@ -7,3 +9,15 @@ function getRandomNumber(min, max) {
     let result = Math.floor(generateNumber) + min; // decimal -> whole, adding min is inclusive of max range
     return result;
 }
+
+function generateLotteryTicket() {
+    let drawnLotteryTicket = []
+    
+    while (drawnLotteryTicket.length != 6) {
+        drawnLotteryTicket.push(getRandomNumber(1,44));
+    }
+    if (drawnLotteryTicket.length == 6) {
+        return drawnLotteryTicket;  
+    }
+}
+
