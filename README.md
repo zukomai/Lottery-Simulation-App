cb0c6113af0fb9c62ef8e8ab6c89e41b608339aa
zukomai <firenationllama@gmail.com>
2022-09-10 16:23:56 -0700

    main app is running but crashing the browser

--- README.md
+++ README.md
@@ -5,10 +5,18 @@ Day 3/100 of *Coding Challenge* ✅
 Javascript app that allows a user to select their own lottery ticket numbers, then have the computer run a simulation of lottery draws until the user's numbers are chosen. 
 
 ## Features
-- Users input six numbers to simulate buying a lottery ticket
-- User submits their numbers and simulation runs, iterating through randomly generated array of six numbers to simulate a lottery drawing
+- Users input six numbers to simulate buying a lottery ticket.
+- User submits their numbers and simulation runs, iterating through randomly generated array of six numbers to simulate a lottery drawing.
 
 ## Lessons Learned
 - [x] How to use `Math.random()` and `Math.floor()` 📝
-- [] Concept of why we add `+1` to the Min and Max for our range....
+- [x] Concept of why we add `+1` to the Min and Max for our range....
 
+----------------------------------------
+
+Day 4/100 of *Coding Challenge* ✅
+
+## Lessons Learned
+- [x] Realized I will need to run while loop on server side when comparing the user and winning lottery ticket. Client side iterations are crashing the browser.
+- [x] Learned how to call button `.onclick ` within the html file.
+- [x] Learned that order of function calling and global v. local variables matter. Preventing issues of variables being null or called before being initialized.
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Lottery Simulation App
 
-
+var drawnLotteryTicket = []
+// const ball1 = document.getElementById('ball1');
+// const ball2 = document.getElementById('ball2');
+// const ball3 = document.getElementById('ball3');
+// const ball4 = document.getElementById('ball4');
+// const ball5 = document.getElementById('ball5');
+// const ball6 = document.getElementById('ball6');
+// const btn = document.getElementById('btn');
 
 // Function that picks 6 random numbers between 1-45.
 function getRandomNumber(min, max) {
@@ -10,8 +17,11 @@ function getRandomNumber(min, max) {
     return result;
 }
 
+// function creates an array, while theres not six numbers in array -> draw more numbers
+// When 6 numbers is in array, return the array
+// New numbers created from emtpy array at beginning of the fuction
 function generateLotteryTicket() {
-    let drawnLotteryTicket = []
+    drawnLotteryTicket = []
     
     while (drawnLotteryTicket.length != 6) {
         drawnLotteryTicket.push(getRandomNumber(1,44));
@@ -20,4 +30,48 @@ function generateLotteryTicket() {
         return drawnLotteryTicket;  
     }
 }
+// Creating a function that is called on button press in index.html and gets uers input from fields
+function getUserTicket() {
+    let userTicket = []
 
+    const ball1 = document.getElementById('ball1').value;
+    const ball2 = document.getElementById('ball2').value;
+    const ball3 = document.getElementById('ball3').value;
+    const ball4 = document.getElementById('ball4').value;
+    const ball5 = document.getElementById('ball5').value;
+    const ball6 = document.getElementById('ball6').value;
+
+    // Pushing values from the input fields to an array in numeric format
+    userTicket.push(
+        Number(ball1),
+        Number(ball2),
+        Number(ball3),
+        Number(ball4),
+        Number(ball5),
+        Number(ball6)
+        )
+
+    // Set local storage for user's lottery number picks
+    localStorage.setItem('ball1', ball1);
+    localStorage.setItem('ball2', ball2);
+    localStorage.setItem('ball3', ball3);
+    localStorage.setItem('ball4', ball4);
+    localStorage.setItem('ball5', ball5);
+    localStorage.setItem('ball6', ball6);
+    
+    function compareTickets() {
+        while (userTicket != drawnLotteryTicket) {
+            generateLotteryTicket();
+            console.log('user ticket was:', userTicket, 'winning ticket was: ', drawnLotteryTicket);
+        }
+        if (userTicket == drawnLotteryTicket) {
+            console.log("Winner!");
+        }
+    }
+
+    // Reset the lottery ball form input after each press of the button
+    //document.getElementById("lottery-form").reset(); 
+    compareTickets();
+}
+
+// Compare user's ticket in local storage to newly generated ticket from array
